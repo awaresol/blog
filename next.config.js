60683b106c30d8a86da18689f4466bbbf6719958
Ahmet Türker <ahmet@awaresol.com>
2022-01-20 22:52:22 +0300

    basepath update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: "/docs",
   images: {
     loader: 'cloudinary',
     path: 'https://res.cloudinary.com/awaresolofficial/image/upload/'

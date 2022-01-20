09aef7f238f2f36e386cf69895cd4681b48b3fec
Ahmet Türker <ahmet@awaresol.com>
2022-01-20 21:46:24 +0300

    added image provider

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    loader: 'cloudinary',
+    path: 'https://res.cloudinary.com/awaresolofficial/image/upload/'
+  },
 }
 
 module.exports = nextConfig
